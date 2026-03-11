2b3b6f3e4652ea52f86ead30b9e4bfe14b820770
baxronovaxror-byte <baxronovaxror@gmail.com>
2026-03-11 16:34:52 +0500

    Initialize Data Warehouse with schemas
    Added schemas for bronze, silver, and gold layers in the Data Warehouse.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,12 @@
+-- Create Database "Data Warehouse"
 
+use master;
+create database Datawarehouse;
+use Datawarehouse;
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
